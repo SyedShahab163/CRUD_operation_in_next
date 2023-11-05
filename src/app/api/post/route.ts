84c5319e74858a378eff33e2a8f19a1f9b8cb065
SyedShahab163 <Syedshahab163@gmail.com>
2023-11-05 12:22:10 -0800

    added in new condition Boolean and Name checks

--- src/app/api/post/route.ts
+++ src/app/api/post/route.ts
@@ -1,41 +1,58 @@
 import { NextResponse } from "next/server";
 import {postuser} from "@/lib/Userdata"
-import { experimental_taintUniqueValue } from "react";
+// import { experimental_taintUniqueValue } from "react";
 
- export  const POST =  async (req:Request, res:Response) => {
+ export const POST =  async (req:Request, res:Response) => {
     
-  try {
-    const {name , age ,id} = await req.json()
+  // try {
+    const {name,age,id ,vip} = await req.json()
     const Userdata = {
         Name:name,
         age:age,
         id:id,
     }
-    postuser(Userdata);
+    postuser(Userdata)
+      let user
+  //  if( age > 18 &&  age < 60){
+      // user = "this is your eligible"
+  //  }
+    // else {
+        //  user = "sorry this is not eligile"
+    // }
+      // 
+    const  Condition: boolean = true; 
+     let con
+       if (Condition === vip) {
+        user = Userdata
+    
+      } else {
    
-      let user 
-      const users= "sorry this is not eligile"
-     
-   if( age > 18 &&  age < 60){
-     user = "this is your eligible"
-   }
-    else {
-         user = "sorry this is not eligile"
+     user = "sorry this is not a vip person"
     }
-    return NextResponse.json({
+      if (name === 'admin') {
+      user ="Name is admin Condition satisfied"
+
+    } else 
+       user = "Name is not admin. Condition not satisfied"
+      
+
+     return NextResponse.json({
         message:"ok",
-        user,
+        user
+        
     },
     {
         status:200
     }
     )
-  } catch (error) {
-     return NextResponse.json({
-         error:"Somethin went Wrong"
-     },
-     {
-         status:500
-    })
-  }   
- }
+  }
+  // } catch (error) {
+    //  return NextResponse.json({
+        //  error:"Somethin went Wrong"
+    //  },
+    //  {
+        //  status:500
+    // })
+  // }   
+//  }
+//  }
